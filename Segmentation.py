05226fa58d446154f017096b6acc8715043d3e42
angelicaCruz <angelicaorigin@gmail.com>
2021-04-16 21:38:33 -0700

    Wrong image bug fix, removal of cuda

--- Segmentation.py
+++ Segmentation.py
@@ -8,6 +8,7 @@ from UNET_MODEL import UNET as nn
 import os
 import numpy as np
 import torch.optim as optim
+from skimage.transform import resize
 from torch.utils.data import Dataset
 import torchvision.transforms.functional as F
 import glob
@@ -27,21 +28,21 @@ class Segmentation:
 class UNET(Segmentation):
     def __init__(self):
         # tim edit - added test stuff to init()
-        DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = nn(in_channels=3, out_channels=1).to(DEVICE)
+        # DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.model = nn(in_channels=3, out_channels=1)
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)
         self.checkpoint = {
             'state_dict': self.model.state_dict(),
             'optimizer': self.optimizer.state_dict()
         }
-        self.load_checkpoint(torch.load('bin_checkpoint.pth.tar', map_location=DEVICE), self.model)
+        self.load_checkpoint(torch.load('bin_checkpoint.pth.tar', map_location='cpu'), self.model)
 
     def transform(self, read_from_path, write_to_path):
         # tim edit - writing to specific file path with full extension, no need to create directory
         #if not os.path.exists(write_to_path):
             #os.mkdir(write_to_path)
 
-        height, width = 640, 360
+        height, width = 360, 640
         transforms = A.Compose([
             #A.Resize(height=height, width=width),
             A.Normalize(
@@ -62,15 +63,15 @@ class UNET(Segmentation):
         self.model.eval()
         with torch.no_grad():
             preds = torch.sigmoid(self.model(image))
-            preds = (preds > 0.5).float()
-            
+            preds = (preds > 0.5).float().squeeze()
+
         # tim edit - cast image to global format (it is messy, there is probably a better way)
         img = preds.numpy()
-        img = np.resize(img, (height, width))
+        img = resize(img, (height, width), preserve_range=True)
         img = Image.fromarray(np.uint8(img*255))
-        img = utils.convertPIL(img)
+        # img = utils.convertPIL(img)
         img.save(write_to_path)
-        #torchvision.utils.save_image(img, write_to_path)
+        # torchvision.utils.save_image(img, f'{write_to_path}/{read_from_path}')
 
 
     def load_checkpoint(self,checkpoint, model):
@@ -78,11 +79,6 @@ class UNET(Segmentation):
         model.load_state_dict(checkpoint["state_dict"])
 
 
-
-
-
-# DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
-# model = nn(in_channels=3, out_channels=4).to(DEVICE)
-# unet = UNET(model, 'my_checkpoint2.pth.tar')
-# unet.transform('tello/runs/tim 12-3-2021 15-25-16/photos/0_takePictures/Scene.png','saved_images')
-
+# # DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+# model = UNET()
+# model.transform('unreal_fire_ex.png', 'saved_images/pred.png')
